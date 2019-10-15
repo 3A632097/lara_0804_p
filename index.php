f6369e9d0ba4ab2923ebc3de20cb8da7d3160975
Ivy Lin <a0981792089@gmail.com>
2019-10-15 16:29:48 +0800

    git init commit & DB setting

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
 require __DIR__.'/bootstrap.php';
-require __DIR__.'/vendor/autoload.php';
-
-use Carbon\Carbon;
-Carbon::setLocale('zh-tw');
 
 // connect to dabase
 try {
@@ -85,7 +81,7 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">文章清單
-                    <small><?php echo Carbon::now()->toDateString(); ?></small>
+                    <small>{今日日期}</small>
                 </h1>
             </div>
         </div>
@@ -105,7 +101,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
+                    發表於 <?=$row->created_at?>
                 </p>
             </div>
             <?php endwhile; ?>
